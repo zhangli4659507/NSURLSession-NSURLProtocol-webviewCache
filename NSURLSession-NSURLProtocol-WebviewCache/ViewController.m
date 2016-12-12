dde1d76a778448c4bbda87666e185b7cc6a15131
Mark <365764028@qq.com>
2016-12-12 14:30:45 +0800

    change

--- NSURLSession-NSURLProtocol-WebviewCache/ViewController.m
+++ NSURLSession-NSURLProtocol-WebviewCache/ViewController.m
@@ -21,46 +21,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     [self actionRefrash:nil];
     
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:nil];
-    NSURLSessionDataTask *data = [session dataTaskWithURL:[NSURL URLWithString:@"http://img.233.com/www/img/index/2016/130X248/jzs1130-248.png"]];
-    [data resume];
-    
     
 }
-
-
-- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
-    
-  
-    // 允许处理服务器的响应，才会继续接收服务器返回的数据
-    completionHandler(NSURLSessionResponseAllow);
-    self.cacheData = [NSMutableData data];
-    }
-
--  (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
-    //下载过程中
-   
-    [self.cacheData appendData:data];
-    
-}
-
-- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
-    //    下载完成之后的处理
-    
-    if (error) {
-       
-    } else {
-        
-//        UIImage *image = [UIImage imageWithData:self.cacheData];
-//        UIImageView *imav = [[UIImageView alloc] init];
-//        imav.frame = CGRectMake(10, 10, 100, 100);
-//        imav.image = image;
-//        [self.view addSubview:imav];
-        //将数据的缓存归档存入到本地文件中
-      
-    }
-}
-
 
 - (IBAction)actionRefrash:(id)sender {
     
